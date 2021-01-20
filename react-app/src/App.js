e83488814de9bae9bc03ed269047c5547767ec05
rdanb <danbirle.dev@gmail.com>
2021-01-20 22:24:55 +0100

    minor fixes

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,6 +3,7 @@ import { ExposureModal } from './components/ExposureModal'
 import { InputField } from './components/InputField/Field'
 import { Button } from './components/Button/Button'
 import { theme } from './util/Theme'
+import { messages } from './util/Messages'
 import * as yup from 'yup'
 import styled from 'styled-components'
 
@@ -37,13 +38,15 @@ function App() {
 
   function validate({target}) {
     const { value } = target
+    // validation for ensuring the 10 characters limit is not passed
     if (value.length > 10) {
-      setValidationErrors('Please provide maximum 10 characters')
+      setValidationErrors(messages.maxCharactersValidation)
       return setInputValue()
     }
 
+    // validation for ensuring the value won't end with "."
     if (value[value.length - 1] === '.') {
-      setValidationErrors('Please provide a valid number')
+      setValidationErrors(messages.numberValidation)
       return setInputValue()
     }
 
@@ -51,14 +54,21 @@ function App() {
       setInputValue(value)
       setValidationErrors()
     }).catch(error => {
-      setValidationErrors('Please provide a valid number')
+      setValidationErrors(messages.numberValidation)
       setInputValue()
     })
   }
 
+  function handleKeyPress(e) {
+    if (e.key === 'Enter' && Boolean(inputValue)) {
+      setPersonId(parseFloat(inputValue))
+      e.target.blur()
+    }
+  }
+
   return (
     <AppWrapper>
-      <InputField placeholder="Please insert a numeric value" type="text" autoFocus onChange={(e) => validate(e)} />
+      <InputField onKeyPress={handleKeyPress} placeholder={messages.textFieldPlaceholder} type="text" autoFocus onChange={(e) => validate(e)} />
       <Button disabled={!inputValue} onClick={() => {
         setPersonId(parseFloat(inputValue))
       }}>Submit</Button>
---
+++ react-app/src/util/Messages.js
@@ -0,0 +1,5 @@
+export const messages = {
+  maxCharactersValidation: "Please provide maximum 10 characters",
+  numberValidation: "Please provide a valid number",
+  textFieldPlaceholder: "Please insert a numeric value",
+}
